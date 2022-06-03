b6a74da9553ee93d3220b9a0828b888380eb0809
hclee <heechul4296@gmail.com>
2022-06-03 12:45:02 +0900

    upload 초기소스

---
+++ src/test/java/study/springboot/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
